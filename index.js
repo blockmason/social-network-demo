139f79aac2e001e7f25e3eda71049b63e9b3db5e
Clayton Savage <clytnsavage@gmail.com>
2019-07-22 18:28:59 -0700

    added in support for Link with Link SDK

--- index.js
+++ index.js
@@ -1,45 +1,110 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    
-    messages = []
-    
+require('dotenv').config();
+
+document.addEventListener("DOMContentLoaded", function (event) {
+const { link } = require('@blockmason/link-sdk');
+const fetch = require('node-fetch');
+
+const project = link({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+}, {
+    fetch
+});
+
+messages = [];
+
+// Set message
+async function postMessage(newMessage) {
+    await project.post('/postMessage', {
+        message: newMessage,
+    }).then(() => {
+        removeMessages();
+        messages = [];
+        
+        var allMessages = project.get('/events/Message').then((message) => {
+            return message.data;
+        });
+
+        allMessages.then(value => {
+            value.forEach(message => {
+                messages.push(message);
+            })
+        }).then(() => {
+            console.log('We have the new messages');
+            console.log(messages);
+            printMessages(messages); 
+        });
+    });
+}
+
+// Get All Messages
+var allMessages = project.get('/events/Message').then((message) => {
+        return message.data;
+    });
+
+
+allMessages.then(value => {
+    value.forEach(message => {
+        messages.push(message);
+    })
+}).then(() => {
+   printMessages(messages); 
+});
+
+
+// Set Profile
+async function setProfile() {
+    await project.post('/setProfile', {
+        displayName: 'Mason',
+        avatarURL: 'https://blockmason.io/wp-content/uploads/2018/11/BMLogo_no-words_IconColor.png0x1111222233334444555566667777888899990000'
+    });
+}
+
+
+// // Get All Profiles
+// project.get('/getProfile').then((message) => {
+//     console.log(message);
+// });
+
+
     feed = document.getElementById('feed');
     textArea = document.getElementById("textarea");
 
     function printMessages() {
-        messages.forEach(element => {
-            var messageText = document.createTextNode(element.message + ' - ');       
-            var messageDate = document.createTextNode(element.timestamp + ' - ');
-            var messageUser = document.createTextNode(element.user);
+        messages.forEach(message => {
+            var messageText = document.createTextNode(message.message + ' - ');
+            var messageDate = document.createTextNode(message.timestamp + ' - ');
+            var messageUser = document.createTextNode(message.senderId);
             var divElement = document.createElement("DIV");
             var pElement = document.createElement("P");
 
-            var messagesFormated = divElement.appendChild(pElement);          
-            
+            var messagesFormated = divElement.appendChild(pElement);
+
             messagesFormated.appendChild(messageText);
             messagesFormated.appendChild(messageDate);
             messagesFormated.appendChild(messageUser);
-            
+
             feed.appendChild(messagesFormated)
         });
     }
 
     function removeMessages() {
+        console.log('Cleared All Messages')
         while (feed.firstChild) {
             feed.removeChild(feed.firstChild);
         }
     }
 
-    document.getElementById("submitMessage").onclick = function() {submitText()};
+    document.getElementById("submitMessage").onclick = function () { submitText() };
 
     function submitText() {
-        if(textArea.value.trim() != ""){
-            messageObject = {message: (textArea.value.trim()), timestamp: Date(), user: 'Network User'};
-            messages.push(messageObject);
-            removeMessages();
-            printMessages();
+        if (textArea.value.trim() != "") {
+            messageObject = textArea.value.trim();
+            postMessage(messageObject);
             textArea.value = "";
         }
     }
 
     printMessages();
+
 });
